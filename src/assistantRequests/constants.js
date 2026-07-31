bc4eeafbcdfc01062d0278718e7d78f3bb52a99d
shinnosuketakeshima <stakesima@gmail.com>
2026-07-31 19:39:43 +0900

    feat: add constants and pure request-field builder for assistant request log

---
+++ src/assistantRequests/constants.js
@@ -0,0 +1,4 @@
+// src/assistantRequests/constants.js
+export const CATEGORIES = ['その他', '座席表作成', '座席表貼り出し', '印刷(講義)', '印刷(講義以外)', '試験監督'];
+export const STATUSES = ['未対応', '受託中', '完了'];
+export const DEFAULT_STATUS = '未対応';
